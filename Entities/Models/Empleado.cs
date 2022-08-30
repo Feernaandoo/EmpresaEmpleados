03314acac7059df21e812d4828cae35e89864424
Fernando Leonel Ríos <fernandorios@live.com.ar>
2022-08-30 00:57:31 -0300

    Creación de biblioteca Entities y de las model classes (entities) Empresa y Empleado.

---
+++ Entities/Models/Empleado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities.Models
+{
+    // Las propiedades se asignarán a las columnas de la base de datos.
+    public class Empleado
+    {
+        [Column("EmpleadoId")]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Nombre is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Nombre is 30 characters.")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Edad is a required field.")]
+        public int Edad { get; set; }
+        [Required(ErrorMessage = "Posicion is a required field.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the Posicion is 20 characters.")]
+        public string Posicion { get; set; }
+        [ForeignKey(nameof(Empresa))]
+        public Guid EmpresaId { get; set; }
+        public Empresa Empresa { get; set; }
+    }
+}
---
+++ Entities/Models/Empresa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities.Models
+{
+    // Las propiedades se asignarán a las columnas de la base de datos.
+    public class Empresa
+    {
+        [Column("EmpresaId")]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Nombre de empresa is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Nombre is 60 characters.")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Dirección de empresa is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Direccion is 60 characters")]
+        public string Direccion { get; set; }
+        public string Pais { get; set; }
+        public ICollection<Empleado> Empleados { get; set; }
+    }
+}
