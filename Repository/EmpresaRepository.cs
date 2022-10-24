da3b08c14332133ea7722f0a29f29485be98c2b8
Fernando Leonel Ríos <fernandorios@live.com.ar>
2022-10-24 19:24:15 -0300

    Creación de las clases EmpleadoRepository y EmpresaRepository. Creación de interfaces IEmpleadoRepository e IEmpresaRepository.

---
+++ Repository/EmpresaRepository.cs
@@ -0,0 +1,17 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class EmpresaRepository : RepositoryBase<Empresa>, IEmpresaRepository 
+    {
+        public EmpresaRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+
+        }
+    }
+}
